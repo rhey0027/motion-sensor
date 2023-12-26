7646fc0cdd987d43565b000f1955a9fa001912d6
rhey_yu <rhey2476@gmail.com>
2023-12-26 03:42:16 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// ----function starts here
+let countEl = 0
+let recordsEl = []
+const hypen = ' - '  
+const disabled = false
+
+//getting id
+const c_element = document.getElementById('countEl')
+const increaseEl = document.getElementById('increase-btn')  
+const decreaseEl = document.getElementById('decrease-btn')
+const totalEl = document.getElementById('total-btn')
+const saveEl = document.getElementById('record-el')
+const outputEl = document.getElementById('output-el')
+const resetEl = document.getElementById('reset-btn')
+
+//rendering data from localStorage
+const renderEntry = (recordsEl) => {
+  let listData = ''
+  for(let i = 0; i < recordsEl.length; i++ ) {
+    listData += `<span>${recordsEl[i]} ${hypen}</span>`
+    const records = recordsEl
+    const totalSum = records.filter((record) => record > 0)
+    .reduce((acc, cur) => acc + cur, 0)
+    outputEl.textContent = totalSum
+    // console.log(totalSum)
+  }
+  saveEl.innerHTML = listData
+}
+
+// fetch record in localStorage
+const getRecords = JSON.parse(localStorage.getItem('recordsEl'))
+if(getRecords) {
+  recordsEl = getRecords
+}
+// button increase function
+increaseEl.addEventListener('mouseover', function() {
+  if(countEl >= 0) {
+    document.getElementById('decrease-btn').disabled = false
+    document.getElementById('total-btn').disabled = false
+    decreaseEl.textContent = 'LEAVE'
+    totalEl.textContent = 'SAVE ENTRY'
+  } 
+  countEl += 1
+  c_element.textContent = countEl
+})
+
+// button decrease function
+decreaseEl.addEventListener('click', function() {
+  if(countEl === 0) {
+    document.getElementById('decrease-btn').disabled = true
+    decreaseEl.textContent = 'DISABLED'
+    countEl = 0
+  } else {
+    countEl -= 1
+    c_element.textContent = countEl
+  }
+})
+
+// onLoad
+window.onload = () => {
+  document.getElementById('total-btn').disabled = true
+  totalEl.textContent = 'DISABLED'
+}
+
+//save data
+totalEl.addEventListener('click', function() {
+  window.onload()
+  let saveRecord = countEl + '-'
+  saveEl.textContent += saveRecord
+  recordsEl.push(countEl)
+  localStorage.setItem('recordsEl', JSON.stringify(recordsEl))
+  renderEntry(recordsEl)
+  c_element.textContent = 0
+  countEl = 0
+}) 
+//reset data
+resetEl.addEventListener('click', function() {
+  localStorage.clear()
+  location.reload()
+})
+
